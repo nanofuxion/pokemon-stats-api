4b78a5be2e940c7452eac93c7501c463bbf0b8c3
Arthur van Acker <arthur@arthurvanacker.com>
2016-08-29 08:56:33 +0100

    add routing

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+//noinspection JSUnresolvedFunction
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.set('port', 8080)
+app.set('port', 8080);
+
+app.get('/', function (req,res) {
+    console.log('Get the homepage');
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/pokemon', function (req,res) {
+    console.log('Get the json');
+    res
+        .status(200)
+        .json({"jsonData": true});
+});
+
+app.get('/file', function (req, res) {
+    console.log('Get the file');
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'), function () {
+    //noinspection JSUnresolvedFunction
     var port = server.address().port;
     console.log('Server is running on port ' + port);
 });
