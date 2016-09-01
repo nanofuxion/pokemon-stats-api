a3cf3144a62e46623f4dacca4f931fd422064d39
Arthur van Acker <arthur@arthurvanacker.com>
2016-09-01 21:59:08 +0100

    add frontend pokemon and types

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+(function(){
+function capitalizeFirstLetter(string) {
+    //noinspection JSUnresolvedFunction
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+var pokemonUrl = 'http://localhost:8080/api/pokemon';
+var typesUrl = 'http://localhost:8080/api/types';
+
+$.get(pokemonUrl, function (data) {
+    data.forEach(function (el) {
+        var div = document.createElement('div');
+        div.className = 'flex-pokemon';
+        if(Array.isArray(el.evolution.names[el.evolution.stage])){
+            div.innerHTML = `${el.pokedex}<br> 
+                             ${el.evolution.names[el.evolution.stage][el.evolution.variation]}<br>
+                             <img class="pokeImg" src="../img/Pokemon/215x215/${el.pokedex.slice(1)}.png" alt="${el.pokedex}">
+                            `;
+        } else {
+            //noinspection JSUnresolvedVariable
+            div.innerHTML = `${el.pokedex}<br> ${el.evolution.names[el.evolution.stage]}<br>
+                             <img class="pokeImg" src="../img/Pokemon/215x215/${el.pokedex.slice(1)}.png" alt="${el.pokedex}">
+                            `;
+        }
+
+
+        document.getElementById('pokemon').appendChild(div);
+    })
+});
+
+$.get(typesUrl, function (data) {
+
+    data.forEach(function(el){
+        var div = document.createElement('div');
+        div.className = 'flex-type';
+        div.innerHTML = capitalizeFirstLetter(el.name);
+        div.style.backgroundColor = el.bgcolor;
+        div.style.color = el.txtcolor || '#f5f5f5';
+        document.getElementById('types').appendChild(div);
+    });
+});
+}());
